7c431d47b9dfface125d7879f5a22a1a678ffae4
Lorenzo Dutra <142991923+iLorenzz@users.noreply.github.com>
2023-12-02 22:17:46 -0300

    Add files via upload

---
+++ directInsertion.cpp
@@ -0,0 +1,31 @@
+#include "sorts.h"
+
+int directInsertion_moves    = 0;
+int directInsertion_compares = 0;
+
+void DirectInsertionsort(String *a, int N)
+{
+    String x;
+
+    for(int i = 2; i < N; i++)
+    {
+        x = a[i];
+        int j = i;
+        
+        a[0] = x;
+        directInsertion_moves++;
+
+        while(x < a[j-1])
+        {
+            a[j-1] = a[j];
+            j -= 1;
+
+            directInsertion_moves++;
+            directInsertion_compares++;
+        }
+        directInsertion_compares++;
+        
+        a[j] = x;
+        directInsertion_moves++;
+    }
+}
